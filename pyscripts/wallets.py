60fed1c9e83ed6008daaf4c1da5a69d0af1dd872
Dominick Labrie <dominicklabrie@mbp.dominick.labrie.ca>
2021-05-30 10:00:03 -0300

    cant spell

--- pyscripts/wallets.py
+++ pyscripts/wallets.py
@@ -5,10 +5,10 @@ wallets = getWallets()
 print("")
 for wallet in wallets:
     if wallet["currency"] == "CAD":
-        print("You have $"+str(wallet["balance"])+" Canadian Rubbles")
+        print("You have $"+str(wallet["balance"])+" Canadian Rupees")
     if wallet["currency"] == "ETH":
-        print("You have "+str(wallet["balance"])+" ETH worth $"+str(round(float(wallet["fiatBalance"]),2))+" Canadian Rubbles")
+        print("You have "+str(wallet["balance"])+" ETH worth $"+str(round(float(wallet["fiatBalance"]),2))+" Canadian Rupees")
     if wallet["currency"] == "BTC":
-        print("You have "+str(wallet["balance"])+" BTC worth $"+str(round(float(wallet["fiatBalance"]),2))+" Canadian Rubbles")
+        print("You have "+str(wallet["balance"])+" BTC worth $"+str(round(float(wallet["fiatBalance"]),2))+" Canadian Rupees")
         
 print("")
