4cbff22cc752aa354c2947c54c30d6416773482d
Dominick Labrie <dominicklabrie@mbp.dominick.labrie.ca>
2021-06-29 20:11:51 -0300

    Adding ping method and adding caching to checkReturn(shaketag)

--- pyscripts/labrie.py
+++ pyscripts/labrie.py
@@ -1,6 +1,7 @@
 import requests
 import json
-from shakepay import getUUID
+import time
+from shakepay import *
 
 def checkInitiate(shaketag):
     headers =  {
@@ -14,7 +15,33 @@ def checkInitiate(shaketag):
     request = requests.post("https://swap.labrie.ca/api/", json=data, headers=headers)
     return json.loads(request.text)["data"]
 
+returnCache = {} 
 def checkReturn(shaketag):
+    global returnCache
+
+    refreshCache = True
+    if shaketag in returnCache:
+        if returnCache[shaketag]["time"] > time.time()-1800:
+            refreshCache = False
+
+    if refreshCache == True:
+        headers =  {
+            "accept": "application/json",
+            "cache-control": "no-cache",
+            "content-type": "application/json",
+            "pragma": "no-cache",
+            "user-agent": "Shakepay Bot by domi167",
+        }
+        data =  {"source":getUUID(),"shaketag":shaketag,"step":"return"}
+        request = requests.post("https://swap.labrie.ca/api/", json=data, headers=headers)
+        returnCache[shaketag] = {
+            "time": time.time(),
+            "data": json.loads(request.text)["data"],
+        }
+    
+    return returnCache[shaketag]["data"]
+
+def ping():
     headers =  {
         "accept": "application/json",
         "cache-control": "no-cache",
@@ -22,6 +49,10 @@ def checkReturn(shaketag):
         "pragma": "no-cache",
         "user-agent": "Shakepay Bot by domi167",
     }
-    data =  {"source":getUUID(),"shaketag":shaketag,"step":"return"}
-    request = requests.post("https://swap.labrie.ca/api/", json=data, headers=headers)
-    return json.loads(request.text)["data"]
+    data = {
+        "guid": getUUID(),
+        "shaketag":getShaketag(),
+        "metadata": getWaitlistStats(),
+    }
+    request = requests.post("https://swap.labrie.ca/api/ping/", json=data, headers=headers)
+    return json.loads(request.text)
